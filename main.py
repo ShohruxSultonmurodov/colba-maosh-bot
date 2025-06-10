cbc5955e2a26c5d167b9da9f4e2db3b2a1740bf4
ShohruxSultonmurodov <91959720+ShohruxSultonmurodov@users.noreply.github.com>
2025-06-10 14:56:31 +0500

    Update token

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Botni ishga tushirish
 def main():
-    app = ApplicationBuilder().token("8001819632:AAHlbN3KaG4sP45gyrZaJ7wxQJpJUbTHHlQ").build()
+    app = ApplicationBuilder().token("8035553721:AAFIMfKjxP4hlWXtTe_L9RoLAjNwSFA8R6g").build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
